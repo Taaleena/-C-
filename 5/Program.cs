f57179c13353dcd07fd83ddf4c8eacd995198520
Talena <tillana@yandex.ru>
2022-06-05 21:20:28 +0300

    summ if - else

---
+++ 5/Program.cs
@@ -0,0 +1,12 @@
+﻿Console.Write("Введите Ваше имя: ");
+string username = Console.ReadLine ();
+
+if(username.ToLower () == "masha")
+{
+     Console.WriteLine("URA");   
+}
+else
+{
+    Console.Write("Privet, ");
+    Console.WriteLine(username);
+}
